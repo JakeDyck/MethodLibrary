f513af4b909df59d3d9943400a0e67bddd82efdc
Dominik Dyck <jdyck@nait.ca>
2023-10-31 09:04:14 -0600

    Updated October 31, 2023

---
+++ MethodLibrary/Program.cs
@@ -0,0 +1,31 @@
+﻿//using System.IO;
+
+namespace MethodLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteFile();
+        }
+
+        static void WriteFile() // File IO --> Writing files
+        {
+            StreamWriter writer = new StreamWriter("some-file.txt"); // replace 'some-file.txt' with desired file name
+            writer.WriteLine("Each 'WriteLine' will save a string to the above file.");
+            writer.Close();
+        }
+
+        static void ReadFile() // File IO --> Reading files
+        {
+            StreamReader reader = new StreamReader("some-file.txt");
+
+            while(reader.EndOfStream == true)
+            {
+                string line = reader.ReadLine();
+                Console.WriteLine(line);
+            }
+            reader.Close();
+        }
+    }
+}
